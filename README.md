926b3181fdf00145c487236ff1ec7ac39b5703c0
tianyi <styzf@qq.com>
2018-09-28 17:20:18 +0800

    微信小程序之一个小需求的UI布局方式

---
+++ README.md
@@ -0,0 +1,25 @@
+# mpvue-ui-demo
+
+> A Mpvue project
+一个小需求的UI布局方式，布局需求：上半部分是一个可变列表，下方底端有一个输入框布局，要求在列表长度较短时，输入框布局保留在底端；当列表长度够长时，输入框布局放在列表底端，可以随列表一起滚动。效果如下图所示：  
+![image](https://github.com/tianyalu/mpvue-ui-demo/blob/master/screenshot/show.gif)  
+</br>
+详情可参考：<a href="https://blog.csdn.net/tianyaluqingchen/article/details/82886852">微信小程序之一个小需求的UI布局方式</a>  
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/pages/uiDemo.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="variable">
+        <div class="btn-container"> 
+          <button class="btn" @click="addItem">add item</button>
+          <button class="btn" @click="reduceItem">reduce item</button>
+        </div>
+        <ul>
+          <li v-for="item in items" :key="item.id">
+            <div class="item">{{item.name}}</div>
+          </li>
+        </ul>
+      </div>
+      <div class="item-input">
+        <input placeholder="填写备注，限制20个字符" maxlength="20" class="comment" v-model.lazy="comment" @change="getComment"/>
+      </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: '测试item' },
+        { name: '测试item' },
+        { name: '测试item' },
+        { name: '测试item' },
+        { name: '测试item' }
+      ]
+    }
+  },
+  methods: {
+    addItem() {
+      this.items.push({ name: '添加的测试item' })
+    },
+    reduceItem() {
+      this.items.splice(this.items.length - 1, 1)
+      console.log('length: ', this.items.length)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+}
+.content{
+  width: 100%;
+  height: 100vh;
+  overflow: scroll;
+}
+.variable {
+  width: 100%;
+  min-height: 86%;
+  flex-direction: column;
+}
+.btn-container {
+  width: 100%;
+  display: flex;
+  margin-bottom: 40rpx;
+}
+.btn {
+  display: flex;
+  flex: 1;
+  margin: 20rpx 40rpx;
+}
+.item {
+  width: 100%;
+  height: 120rpx;
+  line-height: 120rpx;
+  text-align: center;
+  border-bottom: 1px solid #e0e0e0;
+}
+.item-input {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.comment {
+  width: 88%;
+  font-size: 9pt;
+  padding: 10rpx 20rpx;
+  border: 1px solid #e0e0e0;
+  border-radius: 4pt;
+  margin-top: 16rpx;
+  margin-bottom: 10rpx;
+}
+</style>
+
+
